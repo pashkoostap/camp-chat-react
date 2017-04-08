76479c7b13e329cb9d3630a6e37ca7d21063ad89
pashkoostap <pashko.ostap@gmail.com>
2017-04-08 16:41:56 +0300

    included icon-font

--- src/components/core/navigation.jsx
+++ src/components/core/navigation.jsx
@@ -1,25 +1,28 @@
 import React, { Component } from 'react';
+import Style from './navigation.scss';
 
 export default class AppNavigation extends Component {
   render() {
     return (
-      <div className="chat-nav">
-        <nav className="chat-links">
-          <a>Home</a>
-          <a>Chat</a>
-          <a>Users</a>
-          <a>Login</a>
-          <a>Register</a>
-        </nav>
+      <div className="chat-nav-wrap">
+        <div className="chat-nav">
+          <nav className="chat-links">
+            <a>Home</a>
+            <a>Chat</a>
+            <a>Users</a>
+            <a>Login</a>
+            <a>Register</a>
+          </nav>
 
-        <div className="user-profile">
-          <a className="user-profile-link">
-            <span className="user-profile-link__name"></span>
-            <div className="user-profile-link__photo"></div>
-          </a>
-          <button className="log-out  chat-icon-sign-out"></button>
-        </div >
-      </div >
+          <div className="user-profile">
+            <a className="user-profile-link">
+              <span className="user-profile-link__name"></span>
+              <div className="user-profile-link__photo"></div>
+            </a>
+            <button className="log-out  chat-icon-sign-out"></button>
+          </div>
+        </div>
+      </div>
     )
   }
 }
