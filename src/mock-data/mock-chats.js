6a5ba24c46fc1310219063a6011961579eadb12a
pashkoostap <pashko.ostap@gmail.com>
2017-04-10 23:47:33 +0300

    created mock data for chat

---
+++ src/mock-data/mock-chats.js
@@ -0,0 +1,16 @@
+export const CHATS = [
+  {
+    id: 1,
+    name: 'Chatting here',
+    attendees: [1, 2],
+    creator: 1,
+    createdAt: new Date(2017, 3, 10, 20, 0, 0, 0)
+  },
+  {
+    id: 2,
+    name: 'Cool chat',
+    attendees: [3, 4],
+    creator: 3,
+    createdAt: new Date(2017, 3, 10, 20, 10, 0, 0)
+  }
+];
---
+++ src/mock-data/mock-messages.js
@@ -0,0 +1,50 @@
+export const MESSAGES = [
+  {
+    id: 1,
+    senderId: 1,
+    isRead: true,
+    sentAt: new Date(2017, 3, 10, 20, 13, 0, 0),
+    text: 'Hello!',
+    chatId: 1
+  },
+  {
+    id: 2,
+    senderId: 1,
+    isRead: true,
+    sentAt: new Date(2017, 3, 10, 20, 17, 0, 0),
+    text: 'How are you?',
+    chatId: 1
+  },
+  {
+    id: 3,
+    senderId: 2,
+    isRead: true,
+    sentAt: new Date(2017, 3, 10, 20, 27, 0, 0),
+    text: 'Hi man!',
+    chatId: 1
+  },
+  {
+    id: 4,
+    senderId: 2,
+    isRead: true,
+    sentAt: new Date(2017, 3, 10, 20, 37, 0, 0),
+    text: `I\'m fine, thanks!`,
+    chatId: 1
+  },
+  {
+    id: 5,
+    senderId: 3,
+    isRead: true,
+    sentAt: new Date(2017, 3, 10, 20, 47, 0, 0),
+    text: `I\'ll wait you near street at 5 o\'clock`,
+    chatId: 2
+  },
+  {
+    id: 6,
+    senderId: 4,
+    isRead: true,
+    sentAt: new Date(2017, 3, 10, 20, 57, 0, 0),
+    text: `Ok, I will be here in 20 minutes!`,
+    chatId: 2
+  }
+];
