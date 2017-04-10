641c64919f78bf51a5426cdef384e1fc29658de5
pashkoostap <pashko.ostap@gmail.com>
2017-04-10 23:40:36 +0300

    created chat-nav component markup

---
+++ src/components/chats/chat-nav.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import Styles from './chat-nav.scss';
+
+export default class ChatNav extends Component {
+  render() {
+    return (
+      <nav className="left-chat-nav">
+        <button className="left-chat-nav__open  osp-chat-nav-btn  active"></button>
+        <form className="chat-search-panel">
+          <input
+            type="text"
+            className="chat-search-panel__input"
+            name="search" />
+          <button type="submit" className="chat-search-panel__search-btn    osp-chat-nav-btn  chat-icon-search"></button>
+        </form>
+        <button className="left-chat-nav__menu  osp-chat-nav-btn" ></button >
+        <ul className="osp-chat-menu  hidden">
+          <li className="osp-chat-menu__item">
+            <a>New chat</a>
+          </li >
+          <li className="osp-chat-menu__item  osp-chat-menu__item--divider"></li>
+          <li className="osp-chat-menu__item">
+            <a>Menu item</a>
+          </li >
+        </ul >
+      </nav >
+    )
+  }
+}
--- src/components/chats/chats.jsx
+++ src/components/chats/chats.jsx
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
+import Styles from './chats.scss';
+import ChatNav from './chat-nav';
 
 export default class ChatsComponent extends Component {
   render() {
     return (
-      <div className="ct-chats">Chats</div>
+      <div className="ct-chats">
+        <div className="osp-chat">
+          <div className="left-chat-wrap  visible">
+            <ChatNav />
+          </div>
+
+          <div className="right-chat-wrap">
+            das
+          </div>
+        </div>
+      </div>
     )
   }
 }
