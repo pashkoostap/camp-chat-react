ffdc0beee512d64fa4d53ee0b93b66e2e2b38ad3
pashkoostap <pashko.ostap@gmail.com>
2017-04-17 21:10:48 +0300

    created socket connection in root component

--- src/components/root.jsx
+++ src/components/root.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import * as io from 'socket.io-client';
+import { API_CONFIG } from '../api/api-config';
 
 import Header from './core/header';
 import Auth from './auth/auth';
@@ -13,13 +14,16 @@ export default class RootComponent extends Component {
     this.state = {
       text: 'temp'
     }
+    this.socket;
     this.initSocket = this.initSocket.bind(this);
+    this.sendMessage = this.sendMessage.bind(this);
   }
   render() {
     return (
       <div className="app-wrap">
         <Header />
         <button onClick={this.initSocket}>Init Socket</button>
+        <button onClick={this.sendMessage}>Send message</button>
         <Switch>
           <Route exact path='/' component={Home} />
           <Route path='/auth' component={() => { return (<Auth text={this.state.text} />) }} />
@@ -30,6 +34,15 @@ export default class RootComponent extends Component {
   }
   initSocket() {
     console.log('asds')
-    let socket = io.connect('')
+    this.socket = io.connect(API_CONFIG.SOCKET);
+    this.socket.on('connect', () => {
+      console.log(this.socket);
+      this.socket.emit('authenticate', { token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InBhc2hrb09zdGFwIn0.qDBVTt_QG2BB8jnqKvJwUxQTTMQfQnny1_XfoEMGC7w" });
+      console.log(this.socket);
+    })
+    this.socket.on('message', msg => { console.log(msg) })
+  }
+  sendMessage() {
+    this.socket.emit('message', 'Hi')
   }
 }
