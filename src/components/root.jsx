5b5b1dfef86ebf8fdcd5c01d4ee3db0f20e08c51
pashkoostap <pashko.ostap@gmail.com>
2017-05-12 09:43:47 +0300

    fixed root component

--- src/components/root.jsx
+++ src/components/root.jsx
@@ -18,18 +18,12 @@ class RootComponent extends Component {
     this.state = {
       isLoggedUser: this.props.userInfo.user !== undefined
     }
-    this.socket;
 
     this.onUserLogin = this.onUserLogin.bind(this);
     this.onUserLogout = this.onUserLogout.bind(this);
     this.changeIsLoggedState = this.changeIsLoggedState.bind(this);
     this.initSocket = this.initSocket.bind(this);
     this.getSocket = this.getSocket.bind(this);
-  }
-  componentDidMount() {
-    if (this.props.userInfo.token) {
-      this.initSocket(this.props.userInfo.token);
-    }
   }
   render() {
     return (
@@ -60,21 +54,26 @@ class RootComponent extends Component {
       </div>
     )
   }
+  componentDidMount() {
+    if (this.state.isLoggedUser && window.socket == undefined) {
+      this.initSocket(this.props.userInfo.token);
+    }
+  }
   initSocket(JWT) {
-    this.socket = io.connect(API_CONFIG.SOCKET);
-    this.socket.on('connect', () => {
-      this.socket.emit('authenticate', { token: JWT });
-      console.log(this.socket);
+    window.socket = io.connect(API_CONFIG.SOCKET);
+    window.socket.on('connect', () => {
+      window.socket.emit('authenticate', { token: JWT });
+      console.log(window.socket);
     })
-    this.socket.on('message', msg => console.log(msg));
-    // this.socket.on('join', msg => console.log('join', msg));
-    this.socket.on('leave', msg => console.log('leave', msg));
-    // this.socket.on('join-room', msg => console.log('join-room', msg));
-    // this.socket.on('leave-room', msg => console.log('leave-room', msg));
-    this.socket.on('new-chat', chat => console.log('new-chat', chat));
+    window.socket.on('message', msg => console.log(msg));
+    // window.socket.on('join', msg => console.log('join', msg));
+    window.socket.on('leave', msg => console.log('leave', msg));
+    window.socket.on('join-room', msg => console.log('join-room', msg));
+    window.socket.on('leave-room', msg => console.log('leave-room', msg));
+    window.socket.on('new-chat', chat => console.log('new-chat', chat));
   }
   getSocket() {
-    return this.socket;
+    return window.socket;
   }
   onUserLogin(userInfo) {
     this.props.actions.userLogin(userInfo);
@@ -85,6 +84,8 @@ class RootComponent extends Component {
     this.props.actions.userLogout(userInfo);
     localStorage.setItem('userInfo', '');
     this.props.history.push('/auth');
+    setTimeout(() => { window.socket = undefined }, 5000)
+
   }
   changeIsLoggedState() {
     this.setState({ isLoggedUser: !this.state.isLoggedUser });
