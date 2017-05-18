6b3087230d517c6aee08a3d3d3e48bfd1a469db3
pashkoostap <pashko.ostap@gmail.com>
2017-05-18 12:35:23 +0300

    added intro video

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,12 +1,23 @@
-import React, { PropTypes } from 'react';
+import React, { Component } from 'react';
 
-const HomePage = ({ props }) => {
-
-  return (
-    <div className="ct-home">
-      <h1>Welcome to Chat Application</h1>
-    </div>
-  );
-};
+class HomePage extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <div className='ct-home'>
+        <div className='intro-wrap'>
+          <div className='intro-wrap__photo'></div>
+          {/*<video className='intro-wrap__video' autoPlay loop >
+            <source src='assets/video/intro.webm' type='video/webm' />
+          </video>*/}
+          {/*<iframe src="https://www.youtube.com/embed/No3VUocaWMo?autoplay=1&controls=0&loop=1&showinfo=0&modestbranding=1&iv_load_policy=3&rel=0" className='intro-wrap__video' frameBorder="0"></iframe>*/}
+          <iframe src="https://player.vimeo.com/video/217970872?autoplay=1&amp;loop=1&amp;" className='intro-wrap__video' frameBorder="0"></iframe>
+        </div>
+      </div>
+    )
+  }
+}
 
 export default HomePage;
